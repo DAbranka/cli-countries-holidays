a0521f3256ae8722b09cd3a383cf592930169fa0
Abranka <davidabranka@gmail.com>
2022-11-16 16:54:30 +0100

    index and package.json updated

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 #!/usr/bin/env node
-
+const axios = require("axios");
 console.log("Hello, Node.JS!");
 
+const year = 2022;
+const CountryCode = "AT";
+
+async function holidate() {
+  axios
+    .get(`https://date.nager.at/api/v3/PublicHolidays/${year}/${CountryCode}`)
+    .then(function (response) {
+      //en cas de réussite de la requete --> affiche la réponse
+      console.log(response);
+    })
+    .catch(function (error) {
+      //en cas d'erreur affiche "error"
+      console.log(error);
+    })
+    .then(function () {});
+}
+
+holidate();
