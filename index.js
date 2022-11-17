a66075de107272558b5b67bafe755b1d4f511fa5
Abranka <davidabranka@gmail.com>
2022-11-17 11:23:28 +0100

    index.js updated

--- index.js
+++ index.js
@@ -1,22 +1,57 @@
 #!/usr/bin/env node
+
+// EXO
+    // RECUPERER les congés des pays demander
+
+// Pseudo Code
+    // REQUEST = récuperer les données depuis les données de l'api date.nager.at
+    // Verifier le contenu de la réponse avec console.log(reponse)
+    // 
+
+
 const axios = require("axios");
-console.log("Hello, Node.JS!");
 
-const year = 2022;
-const CountryCode = "AT";
 
-async function holidate() {
-  axios
-    .get(`https://date.nager.at/api/v3/PublicHolidays/${year}/${CountryCode}`)
-    .then(function (response) {
-      //en cas de réussite de la requete --> affiche la réponse
-      console.log(response);
-    })
-    .catch(function (error) {
-      //en cas d'erreur affiche "error"
-      console.log(error);
-    })
-    .then(function () {});
+let country = process.argv[2].toLowerCase();
+let code;
+
+async function countries() {
+  return await axios
+    .get("https://date.nager.at/api/v3/AvailableCountries")
+    .then((response) => {
+      const data = response.data;
+      return data;
+    });
 }
 
-holidate();
+async function CountryCode() {
+  const countryData = await countries();
+  for (let i = 0; countryData.length; i++) {
+    if (countryData[i]?.name?.toLowerCase() === country) {
+      code = countryData[i].countryCode;
+      return code;
+    }
+  }
+}
+
+async function holidays() {
+  let countryCode = await CountryCode();
+  let currentYear = new Date().getFullYear();
+  let holidaysList = await axios.get(
+    "https://date.nager.at/api/v3/PublicHolidays/" +
+      currentYear +
+      "/" +
+      countryCode
+  );
+  let allholidays = holidaysList["data"];
+  allholidays.forEach((element) => {
+    console.log(
+      element["date"] + " " + element["localName"] + " " + element["name"]
+    );
+  });
+}
+
+// APPEL des fonctions
+countries();
+CountryCode();
+holidays();
